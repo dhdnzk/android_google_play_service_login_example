b19e3493d82977837cbfd258b5f9edb87d50072c
Donghoon Im <dongttang@gmail.com>
2017-09-19 15:53:11 +0900

    .

--- app/src/main/java/com/dongttang/androidloginexampleviagoogleplayservice/MainActivity.java
+++ app/src/main/java/com/dongttang/androidloginexampleviagoogleplayservice/MainActivity.java
@@ -1,10 +1,9 @@
 package com.dongttang.androidloginexampleviagoogleplayservice;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -139,9 +138,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         startActivity(intent);
 
     }
-
-
-
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
